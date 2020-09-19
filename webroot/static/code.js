cf9da598ac3af65a3ff6c3100a89ba9816cb07e8
Reynaldo Fabrinny <reynaldo.fabrinny@gmail.com>
2020-09-19 17:42:43 +0200

    Improving Front-end.  - Adjusting code.js to display creation date and name.  - Displaying the new Information on the index.html.

--- webroot/static/code.js
+++ webroot/static/code.js
@@ -6,22 +6,37 @@ fetch(servicesRequest)
     })
     .then(function (serviceList) {
         serviceList.forEach(service => {
-
             var li = document.createElement("li");
-            li.appendChild(document.createTextNode(service.name + ': ' + service.status));
+            var serviceRow = "URL: " + service.url;
+
+            if (service.name) {
+                serviceRow += " | Name: " + service.name;
+            }
+            if (service.status_response === 0) {
+                serviceRow += " | Status Response: OK";
+            } else if (service.status_response === 1) {
+                serviceRow += " | Status Response: FAIL";
+            } else {
+                serviceRow += " | Status Response: NOT CHECKED YET";
+            }
+            if (service.creation_date) {
+                serviceRow += " | Creation Date: " + service.creation_date;
+            }
+            li.appendChild(document.createTextNode(serviceRow));
             listContainer.appendChild(li);
         });
     });
 
 const saveButton = document.querySelector('#post-service');
 saveButton.onclick = evt => {
-    let urlName = document.querySelector('#url-name').value;
+    let serviceUrl = document.querySelector('#service-url').value;
+    let serviceName = document.querySelector('#service-name').value;
     fetch('/service', {
         method: 'post',
         headers: {
             'Accept': 'application/json, text/plain, */*',
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({url: urlName})
+        body: JSON.stringify({url: serviceUrl, name: serviceName})
     }).then(res => location.reload());
 }
