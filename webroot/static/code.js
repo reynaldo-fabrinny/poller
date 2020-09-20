b6d34f8b455f65736fc678e1aa57b33bbc4118d0
Reynaldo Fabrinny <reynaldo.fabrinny@gmail.com>
2020-09-20 15:45:09 +0200

    Improving front-end
     - Adding Bootstrap
     - Adding JQuery

--- webroot/static/code.js
+++ webroot/static/code.js
@@ -1,37 +1,63 @@
-const listContainer = document.querySelector('#service-list');
+const $tableBody = $("#table-body");
 let servicesRequest = new Request('/service');
+
 fetch(servicesRequest)
     .then(function (response) {
         return response.json();
     })
     .then(function (serviceList) {
         serviceList.forEach(service => {
-            var li = document.createElement("li");
-            var serviceRow = "URL: " + service.url;
+            let $tr = $('<tr/>').addClass("d-flex");
 
-            if (service.name) {
-                serviceRow += " | Name: " + service.name;
-            }
-            if (service.status_response === 0) {
-                serviceRow += " | Status Response: OK";
-            } else if (service.status_response === 1) {
-                serviceRow += " | Status Response: FAIL";
+            let $tdUrl = $('<td/>');
+            $tdUrl.addClass("col-4").append(service.url);
+            $tr.append($tdUrl);
+
+            let $tdName = $('<td/>');
+            $tdName.addClass("col-3").append(service.name);
+            $tr.append($tdName);
+
+            let $tdStatus = $('<td/>');
+            $tdStatus.addClass("col-1");
+            if (service.status_response === 200) {
+                $tdStatus.append("OK");
+            } else if (service.status_response) {
+                $tdStatus.append("FAIL");
             } else {
-                serviceRow += " | Status Response: NOT CHECKED YET";
+                $tdStatus.append("NOT CHECKED YET");
             }
-            if (service.creation_date) {
-                serviceRow += " | Creation Date: " + service.creation_date;
-            }
-            li.appendChild(document.createTextNode(serviceRow));
-            listContainer.appendChild(li);
+            $tr.append($tdStatus);
+
+            let $tdCreationDate = $('<td/>');
+            $tdCreationDate.addClass("col-2");
+            $tdCreationDate.append(service.creation_date);
+            $tr.append($tdCreationDate);
+
+            let $tdButton = $('<td/>');
+            let $deleteBtn = $('<button/>')
+                .text('Remove')
+                .attr("service-id", service.id)
+                .click(function () {
+                    let serviceId = $(this).attr("service-id");
+                    fetch('/remove/' + serviceId, {
+                        method: 'delete',
+                        headers: {
+                            'Accept': 'application/json, text/plain, */*',
+                            'Content-Type': 'application/json'
+                        }
+                    }).then(res => location.reload());
+                }).addClass("btn btn-sm btn-danger");
+            $tdButton.append($deleteBtn);
+            $tr.append($tdButton);
+
+            $tableBody.append($tr);
         });
     });
 
-const saveButton = document.querySelector('#post-service');
-saveButton.onclick = evt => {
+$('#post-service').click(function () {
     let serviceUrl = document.querySelector('#service-url').value;
     let serviceName = document.querySelector('#service-name').value;
-    fetch('/service', {
+    fetch('/add', {
         method: 'post',
         headers: {
             'Accept': 'application/json, text/plain, */*',
@@ -39,4 +65,8 @@ saveButton.onclick = evt => {
         },
         body: JSON.stringify({url: serviceUrl, name: serviceName})
     }).then(res => location.reload());
-}
+});
+
+$(document).ready(function(){
+
+});
