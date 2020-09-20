4a111ee898636164414b6317391d69b3546b78da
Reynaldo Fabrinny <reynaldo.fabrinny@gmail.com>
2020-09-20 15:45:34 +0200

    Adding Test to the URL validation.

--- src/test/java/se/kry/codetest/TestMainVerticle.java
+++ src/test/java/se/kry/codetest/TestMainVerticle.java
@@ -10,31 +10,69 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import se.kry.codetest.utils.Constants;
+import se.kry.codetest.utils.Utils;
 
 import java.util.concurrent.TimeUnit;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * The type Test main verticle.
+ */
 @ExtendWith(VertxExtension.class)
 public class TestMainVerticle {
 
+
+    /**
+     * Deploy verticle.
+     *
+     * @param vertx       the vertx
+     * @param testContext the test context
+     */
     @BeforeEach
     void deploy_verticle(Vertx vertx, VertxTestContext testContext) {
         vertx.deployVerticle(new MainVerticle(), testContext.succeeding(id -> testContext.completeNow()));
     }
 
+    /**
+     * Start http server.
+     *
+     * @param vertx       the vertx
+     * @param testContext the test context
+     */
     @Test
-    @DisplayName("Start a web server on localhost responding to path /service on port 8080")
+    @DisplayName("Start a web server on localhost responding to path /service on the default port " + Constants.DEFAULT_PORT)
     @Timeout(value = 10, timeUnit = TimeUnit.SECONDS)
     void start_http_server(Vertx vertx, VertxTestContext testContext) {
         WebClient.create(vertx)
-                .get(8080, "::1", "/service")
+                .get(Constants.DEFAULT_PORT, "::1", "/service")
                 .send(response -> testContext.verify(() -> {
                     assertEquals(200, response.result().statusCode());
                     JsonArray body = response.result().bodyAsJsonArray();
                     assertEquals(1, body.size());
                     testContext.completeNow();
                 }));
+    }
+
+    @Test
+    @DisplayName("Tests the isValidUrl method from the Utils.")
+    void validateURL() {
+        // Valid URLs
+        assertTrue(Utils.isValidUrl("https://www.kry.se/"));
+        assertTrue(Utils.isValidUrl("https://www.youtube.com/"));
+        assertTrue(Utils.isValidUrl("https://thisisrosa.com/"));
+        assertTrue(Utils.isValidUrl("https://stackoverflow.com/"));
+        assertTrue(Utils.isValidUrl("https://www.google.com.br/"));
+
+
+        //Invalid URLs
+        assertFalse(Utils.isValidUrl("kry.se"));
+        assertFalse(Utils.isValidUrl("sdgf"));
+        assertFalse(Utils.isValidUrl(""));
+        assertFalse(Utils.isValidUrl("HTT:/google.com"));
+        assertFalse(Utils.isValidUrl("bbc.com"));
+        assertFalse(Utils.isValidUrl("httsp://google.com"));
     }
 
 }
