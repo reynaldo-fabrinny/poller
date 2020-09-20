f5b39c403950893caf4e52fc80a2bdd739a1352c
Reynaldo Fabrinny <reynaldo.fabrinny@gmail.com>
2020-09-20 15:45:53 +0200

    Creating URL Validation method

--- src/main/java/se/kry/codetest/utils/Utils.java
+++ src/main/java/se/kry/codetest/utils/Utils.java
@@ -1,5 +1,7 @@
 package se.kry.codetest.utils;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
@@ -22,5 +24,23 @@ public class Utils {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern(Constants.DATE_PATTERN);
 
         return ldt.format(formatter);
+    }
+
+    /**
+     * Is valid url boolean.
+     *
+     * @param url the url
+     * @return the boolean
+     */
+    public static boolean isValidUrl(String url) {
+        try {
+            URI parsedUrl = new URI(url).parseServerAuthority();
+            if (parsedUrl.getScheme() == null || (!parsedUrl.getScheme().equals("http") && !parsedUrl.getScheme().equals("https"))) {
+                return false;
+            }
+        } catch (URISyntaxException e) {
+            return false;
+        }
+        return true;
     }
 }
